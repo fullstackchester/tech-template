4ce6b587fa6e35838cad715e012c5ec11b5a52e3
Mark Chester Danday <mark.chester.danday@gmail.com>
2023-01-22 11:26:14 +0800

    toolbar and footer setup

---
+++ public/src/components/footer/footer.js
@@ -0,0 +1,13 @@
+document.write(
+    `
+    <footer>
+        <div class="footer-links">
+            <span>Follow us on our social media</span>
+            <h1>Fb</h1>
+        </div>
+        <div class="copyrights">
+            Balagtas National Agricultural High School © All Rights Reserved, 2023.
+        </div>
+    </footer>
+    `
+)
---
+++ public/src/components/toolbar/toolbar.js
@@ -0,0 +1,11 @@
+document.write(`
+    <nav class="main-toolbar">
+        <span class="brand">Branding</span>
+        <span class="spacer"></span>
+        <ul class="menu-link">
+            <li class="menu-link-item">Home</li>
+            <li class="menu-link-item">About</li>
+            <li class="menu-link-item">Contact us</li>
+        </ul>
+    </nav>
+`);
